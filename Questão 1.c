8a974013c0fbf7669647525d7a655a833b52f555
InfratorXD <guiguicazeee@gmail.com>
2023-10-18 20:24:35 -0300

    lista 2

---
+++ Questão 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+    int main(){
+    int valor[10];
+    int tamanho = 10;
+
+    for(int i=0; i<10;i++){
+    scanf("%d",&valor[i]);
+    
+    }
+
+    while(tamanho > 1){
+
+    for(int i=0;i< tamanho;i++){
+        printf("%d ",valor[i]);
+    
+    }
+
+ printf("\n");
+
+    for(int i=0;i < tamanho - 1;i++){
+        valor[i] = valor[i] + valor[i + 1];
+    }
+
+tamanho--;
+
+    }
+        printf("%d",valor[0]);
+
+        return 0;
+    }
